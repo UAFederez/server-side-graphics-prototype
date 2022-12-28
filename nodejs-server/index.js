f875c97def643c964b647762d4e55d75c67208e1
UAFederez <uafederez@gmail.com>
2022-12-28 22:48:23 +0800

    Updated code to communicate to the render server, added an animate button

--- nodejs-server/index.js
+++ nodejs-server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const net = require("net");
 const http = require("http");
 
 const server = http.createServer(app);
@@ -7,10 +8,64 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+function collectArraysAsOneBuffer(arrays) {
+    const byteBuffers = arrays.map((array) => new Uint8Array(array.buffer));
+    const totalSize = byteBuffers.reduce(
+        (accumulated, buffer) => accumulated + buffer.length,
+        0
+    );
+    const mergedBuffers = new Uint8Array(totalSize);
+    byteBuffers.reduce((accumulated, buffer) => {
+        mergedBuffers.set(buffer, accumulated);
+        return accumulated + buffer.length;
+    }, 0);
+    return mergedBuffers;
+}
+
+const RADIUS = 2.0;
+
 io.on("connection", (socket) => {
     console.log("Client connected");
     socket.on("requestFrame", (info) => {
-        console.log(info);
+        const renderSocket = net.connect(
+            { host: "127.0.0.1", port: 3434 },
+            () => {
+                console.log("Connected to server");
+                const imageDimensions = Uint32Array.from([
+                    info.width,
+                    info.height,
+                ]);
+                console.log(info);
+                const lightPosition = Float32Array.from([
+                    Math.sin(info.idx) * RADIUS,
+                    2.0,
+                    Math.cos(info.idx) * RADIUS,
+                ]);
+                const spherePosition = Float32Array.from([0, 0.0, 2.0]);
+
+                const message = collectArraysAsOneBuffer([
+                    imageDimensions,
+                    lightPosition,
+                    spherePosition,
+                ]);
+                renderSocket.write(message);
+            }
+        );
+
+        renderSocket.on("data", (data) => {
+            console.log("Received response");
+            socket.emit("responseFrame", {
+                error: null,
+                data: data.toString(),
+            });
+        });
+
+        renderSocket.on("error", (error) => {
+            socket.emit("responseFrame", {
+                error: `No connection: ${error}`,
+                data: [],
+            });
+        });
     });
 });
 
